21a1bc18d9e93f4128596d709d9e7965d2a50600
henry <henryfan@msn.com>
2016-04-01 11:40:53 +0800

    no message

---
+++ SmartRoute.WebApi4Grid.Test.WebApiServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using SmartRoute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartRoute.WebApi4Grid.Test.WebApiServer
+{
+    class Program : SmartRoute.WebApi4Grid.ApiController
+    {
+
+        private static SmartRoute.WebApi4Grid.ApiCenter mCenter;
+
+        static void Main(string[] args)
+        {                
+            mCenter = new SmartRoute.WebApi4Grid.ApiCenter();
+            mCenter.Register(new Program());
+            Console.WriteLine("api service start!");
+            System.Threading.Thread.Sleep(-1);
+        }
+
+        [WebApi("/TestApi/Login")]
+        public object Login(string username, string pwd)
+        {
+            object result= new { UserName = username, PWD = pwd };
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(result));
+            return result;
+        }
+        [WebApi("/TestApi/Register")]
+        public object Register(string username, string email, string pwd)
+        {
+            object result= new { UserName = username, EMail = email, PWD = pwd };
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(result));
+            return result;
+        }
+
+    }
+}
